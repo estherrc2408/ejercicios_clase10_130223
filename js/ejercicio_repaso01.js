cae7bc4ade2d12a5e41d5e1cd78288b0ddc1a355
estherrc2408 <badger24a@gmail.com>
2023-02-13 19:35:02 +0100

    ejercicio repaso hecho de nuevo

--- js/ejercicio_repaso01.js
+++ js/ejercicio_repaso01.js
@@ -1,13 +1,28 @@
-
-var nota1 = parseFloat(prompt("Introduce la primera nota"));
-var nota2 = parseFloat(prompt("Introduce la segunda nota"));
-var nota3 = parseFloat(prompt("Introduce la tercera nota"));
-console.log(nota1, nota2, nota3);
-
+/*
+1º Solicite al usuario el ingreso por teclado de 3 notas
+2º Saque la media de las notas
+3º Muestre por pantalla
+si la nota es menor que 5 'SUSPENSO'
+si la nota es mayor o igual 5 y menor que 7 'APROBADO'
+si es mayor o igual que 7 y menor o igual que 10 'sobresaliente'
+*/
+let notas = [];
+function introducirnotas() {
+    while (notas.length < 3) {
+        var nota = parseFloat(prompt("Introduce una nota entre el 0 y el 10"));
+        if(nota<0 || nota>10){
+            alert("la nota introducida no está entre 0 y 10");
+        }else{
+            notas.push(nota);
+        }        
+    }
+}
+introducirnotas();
+alert("Las notas introducidas son: "+notas);
 
 function calculanotas() {
     let media;
-    media = (nota1 + nota2 + nota3) / 3;
+    media = (notas[0] + notas[1] + notas[2]) / 3;
     console.log(media);
     if (media < 5) {
         alert("La media de las notas introducidas es: " + media + ", estás suspenso");
